e37190f9d9c0fd5d1af9b784509330b6d3138b53
mishra-rishabh <rishabh.ramg@gmail.com>
2024-01-07 20:58:48 +0530

    todo app final and react icons library

---
+++ projects/06_todo_app_v3/README.md
@@ -0,0 +1,5 @@
+# Todo App Version 1
+
+* This version only contains UI of the project and not any functionality.
+* It is improved as redundancy is removed.
+* It is using props and css module to make code more cleaner.
---
+++ projects/06_todo_app_v3/src/components/WelcomeMessage.jsx
@@ -0,0 +1,7 @@
+import styles from "./css_module/WelcomeMessage.module.css";
+
+function WelcomeMessage() {
+    return <p className={styles.welcome}>Enjoy bro, kuchh task add karo!</p>
+}
+
+export default WelcomeMessage;
