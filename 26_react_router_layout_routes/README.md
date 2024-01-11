2925bcaa0d11d46c98df8dc36dd9205287018f71
mishra-rishabh <rishabh.ramg@gmail.com>
2024-01-12 00:30:13 +0530

    layout routes

---
+++ 26_react_router_layout_routes/README.md
@@ -0,0 +1,4 @@
+# Layout Routes
+
+1. Layout routes help us to use shared elements
+2. Outlet component is used to render the children at the correct places.
---
+++ 26_react_router_layout_routes/src/components/PostList.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useContext } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
+
+const PostList = () => {
+    const { postList, fetching } = useContext(PostListData);
+    
+
+    /* const handleGetPostsClick = () => {
+        fetch('https://dummyjson.com/posts')
+            .then(res => res.json())
+            .then(data => {
+                addInitialPosts(data.posts);
+            });
+    } */
+
+    return (
+        <>
+            {fetching && <LoadingSpinner/>}
+            {!fetching && postList.length === 0 && <WelcomeMessage /* onGetPostsClick={handleGetPostsClick} */></WelcomeMessage>}
+            {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
+        </>
+    );
+}
+
+export default PostList;
